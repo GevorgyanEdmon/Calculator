a001e8d25b98db4dbbde645a3f4789d0766ada3f
Gigiboba <Gigiboba@HOME-PC>
2026-05-08 18:04:27 +0300

    разделил все на функции, теперь приоритеты операций работают корректно

--- main.cpp
+++ main.cpp
@@ -1,88 +1,104 @@
 #include <iostream>
-#include <stdexcept>
 
 using namespace std;
 
-double result = 0;
-double term = 0;
-char last_sign = '+';
-double num = 0;
+char text[100];
+int pos = 0;
+
+double x1 = 0;
+double x2 = 0;
 char op = ' ';
 
-void ReadFirstNumber() {
-    cin >> result;
-    term = result;
-    result = 0;
+
+double BuildNumber() {
+    int arrInt[50];
+    int countInt = 0;
+    int arrFrac[50];
+    int countFrac = 0;
+    int hasDot = 0;
+
+    while ((text[pos] >= '0' && text[pos] <= '9') || text[pos] == '.') {
+
+        char symbol = text[pos];
+
+        if (symbol == '.') {
+            hasDot = 1;
+        }
+        else {
+            int cifra = symbol - '0';
+
+            if (hasDot == 0) {
+                arrInt[countInt] = cifra;
+                countInt = countInt + 1;
+            } else {
+                arrFrac[countFrac] = cifra;
+                countFrac = countFrac + 1;
+            }
+        }
+        pos = pos + 1;
+    }
+
+    double finalNumber = 0;
+    int i = 0;
+    while (i < countInt) {
+        int power = (countInt - 1) - i;
+        double multiplier = 1;
+        int k = 0;
+        while (k < power) {
+            multiplier = multiplier * 10;
+            k = k + 1;
+        }
+        finalNumber = finalNumber + (arrInt[i] * multiplier);
+        i = i + 1;
+    }
+
+    i = 0;
+    while (i < countFrac) {
+        int power = i + 1;
+        double divider = 1;
+        int k = 0;
+        while (k < power) {
+            divider = divider * 10;
+            k = k + 1;
+        }
+        finalNumber = finalNumber + (arrFrac[i] / divider);
+        i = i + 1;
+    }
+
+    return finalNumber;
 }
 
-bool ReadNextStep() {
-
-    if (!(cin >> op)) return false;
-
-    if (op != '+' && op != '-' && op != '*' && op != '/') {
-        throw runtime_error("Unknown operation sign");
-    }
-
-    if (!(cin >> num)) return false;
-
-    return true;
-}
-
-bool IsEnd() {
-    return cin.peek() == '\n';
-}
-
-void DoMath() {
-    if (op == '*') {
-        term = term * num;
-    }
-    else if (op == '/') {
-        if (num == 0) throw runtime_error("Division by zero");
-        term = term / num;
-    }
-    else if (op == '+' || op == '-') {
-        if (last_sign == '+') result = result + term;
-        if (last_sign == '-') result = result - term;
-        last_sign = op;
-        term = num;
-    }
-
-}
-
-void FinalizeMath() {
-    if (last_sign == '+') result = result + term;
-    if (last_sign == '-') result = result - term;
-}
-
-void StartCalculator() {
-    ReadFirstNumber();
-
-    if (cin.fail()) throw runtime_error("The expression must start with a number");
-
-    while (ReadNextStep()) {
-        DoMath();
-
-        if (IsEnd()) break;
-    }
-
-    if (cin.fail()) {
-        throw runtime_error("An invalid character has been entered");
-    }
-
-    FinalizeMath();
+void Calc() {
+    if (op == '+') x1 = x1 + x2;
+    if (op == '-') x1 = x1 - x2;
+    if (op == '*') x1 = x1 * x2;
+    if (op == '/') x1 = x1 / x2;
 }
 
 
 int main() {
-    cout << "Enter expression: ";
 
-    try {
-        StartCalculator();
-        cout << "Final Result: " << result << endl;
+    cout << "Введите выражение (без пробелов): ";
+    cin >> text;
+
+    x1 = BuildNumber();
+
+    while (true) {
+
+        op = text[pos];
+
+        if (op == '\0') {
+            break;
+        }
+
+        pos = pos + 1;
+
+        x2 = BuildNumber();
+
+        Calc();
     }
-    catch (const exception& error) {
-        cout << "\nerror: " << error.what() << endl;
-    }
+
+    cout << "ответ: " << x1 << endl;
 
     return 0;
 }
